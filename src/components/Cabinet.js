45a9c53d0de66b51aa6bc7850ea2032751241e05
Kiefe <56786730+ArtemSamarskiy@users.noreply.github.com>
2023-06-21 22:47:43 +0300

    Ага

--- src/components/Cabinet.js
+++ src/components/Cabinet.js
@@ -13,27 +13,24 @@ function get_data() {
 
 class Cabinet extends React.Component {
     render() {
-        const cabinet_data = React.createRef(null);
-        let response = get_data();
-        console.log(response.user)
+        let response = get_data().user;
 
         if (!Cookies.get("user_auth")) {
            return <Navigate to={"/"} />;
         }
+
         return (
             <div>
                 Этот кабинет просто имба!
                 <br/>
-                <img height={100} width={100} alt={"avatar"} src={"https://id.api.fronic.ru/"+response.user.avatar_url} />
+                    <img height={100} width={100} alt={"avatar"} src={"https://id.api.fronic.ru/"+response.avatar_url} />
                 <br/>
-                <div ref={cabinet_data} className={"flex gap-5"}>
-                    <UserData _key={"id"} _value={response.user.id} />
-                    <UserData _key={"discord_id"} _value={response.user.discord_id} />
-                    <UserData _key={"name"} _value={response.user.name} />
-                    <UserData _key={"email"} _value={response.user.email} />
-                    <UserData _key={"lang"} _value={response.user.lang} />
-                    <UserData _key={"createdAt"} _value={response.user.createdAt} />
-                    <UserData _key={"updatedAt"} _value={response.user.updatedAt} />
+                <div className={"flex gap-5"}>
+                    {
+                        Object.entries(response).map(data => {
+                            return <UserData _key={data[0]} _value={data[1]} />
+                        })
+                    }
                 </div>
             </div>
         )
